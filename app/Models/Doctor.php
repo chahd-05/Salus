56b86208236c258e21f58666d455dd7f1028f463
shahd <mazouzchahd12@gmail.com>
2026-03-17 11:14:17 +0000

    feat: add Doctor model with fillable fields and appointments relationship

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    //
+    protected $fillable = [
+        'specialty',
+        'city',
+        'yearsOfExperience',
+        'consultationPrice',
+        'availableDays'
+    ];
+
+    public function appointments() {
+        return $this->hasMany(Appointment::class);
+    }
 }
