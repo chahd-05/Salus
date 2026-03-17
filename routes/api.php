3aec58e0e1c8ccce641ad95c7a27ff9b5de33fa9
shahd <mazouzchahd12@gmail.com>
2026-03-17 11:14:52 +0000

    feat: organize API routes by grouping authenticated routes and adding DoctorController endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\AppointmentController;
 use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\Api\DoctorController;
+use App\Http\Controllers\Api\SymptomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +14,13 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->group(function(){
+Route::apiResource('symptoms', SymptomController::class);
+Route::apiResource('appointments', AppointmentController::class);
+});
+
+Route::get('/doctors', [DoctorController::class, 'index']);
+Route::get('/doctors/{id}', [DoctorController::class, 'show']);
+Route::get('/doctors/search', [DoctorController::class, 'search']);
