cc9a45f90267f12f77e726a461aadd450854d8e0
shahd <mazouzchahd12@gmail.com>
2026-03-17 11:14:31 +0000

    feat: add Appointment model with fillable fields and relationships to User and Doctor

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    //
+    protected $fillable = [
+        'appointmentDate',
+        'status',
+        'note'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function doctor() {
+        return $this->belongsTo(Doctor::class);
+    }
 }
