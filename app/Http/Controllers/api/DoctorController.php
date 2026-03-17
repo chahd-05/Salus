ae89d78a576a4742d684626f93e332796c87fe2d
shahd <mazouzchahd12@gmail.com>
2026-03-17 15:07:11 +0000

    fix: implement DoctorController methods for listing, showing, and searching doctors

--- app/Http/Controllers/api/DoctorController.php
+++ app/Http/Controllers/api/DoctorController.php
@@ -3,9 +3,38 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Doctor;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
 class DoctorController extends Controller
 {
-    //
+    use ApiResponse;
+
+    public function index() {
+        $doctors = Doctor::All();
+        return $this->success($doctors, "doctors list");
+    }
+
+    public function show() {
+        $doctor = Doctor::find($id);
+        if(!$doctor){
+            return $this->error(null, "doctor not found");
+        }
+        return $this->success($doctor, "doctor details");
+    }
+
+    public function search(Request $request){
+        $query = Doctor::query();
+
+        if($request->has('specialty')){
+            $query->where('specialty', 'like', '%' .$request->specialty. '%');
+        }
+
+        if($request->has('city')){
+            $query->where('city', 'like', '%' .$request->city .'%');
+        }
+        $doctors = $query->get();
+        return $this->success($doctors, "search results");
+    }
 }
