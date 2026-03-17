d5ec0ffe72af25f9daf92e7f92ad434e3a13f2b0
shahd <mazouzchahd12@gmail.com>
2026-03-17 11:15:52 +0000

    feat: implement SymptomController with CRUD methods

---
+++ app/Http/Controllers/api/SymptomController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSymptomRequest;
+use Illuminate\Http\Request;
+use App\Traits\ApiResponse;
+
+class SymptomController extends Controller
+{
+    use ApiResponse;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSymptomRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
